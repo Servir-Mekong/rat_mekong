ed69da5e34befaa05dc559f6a3e490032a078a9e
Kamalh27 <plnkamalhosen@gmail.com>
2023-07-18 15:22:52 +0700

    add footnote

--- static/js/map_v2.js
+++ static/js/map_v2.js
@@ -952,6 +952,7 @@ fetch(resData)
                     document.querySelector('#resInfo tbody #resLocation').innerHTML = flag;
                     document.querySelector('#resInfo tbody #resLat').innerHTML = lat;
                     document.querySelector('#resInfo tbody #resLng').innerHTML = lng;
+                    document.querySelector('#resInfo tbody #resStatus').innerHTML = status;
                     document.querySelector('#resInfo tbody #resYear').innerHTML = year;
                     document.querySelector('#resInfo tbody #resArea').innerHTML = area;
                     document.querySelector('#resInfo tbody #resCap').innerHTML = cap;
